f15d82f2851a1c75c5c99649e4665a6173743bc1
Abdallah Mahameed <a.mahameed0000@gmail.com>
2024-06-19 17:07:41 +0300

    created map view

--- Landmarks/Landmark.swift
+++ Landmarks/Landmark.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  Landmark.swift
 //  Landmarks
 //
 //  Created by AbdallahMahameed on 19/06/2024.
@@ -10,19 +10,39 @@ import SwiftUI
 struct Landmark: View {
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .withCustomFont(name: Constants.fonts.robotoLight, font: .title)
+        VStack {
+            MapView()
+                .frame(height: 300)
             
-            HStack {
-                Text("Joshua Tree National Park")
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .withCustomFont(name: Constants.fonts.robotoLight, font: .title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .withCustomFont(name: Constants.fonts.robotoLight, font: .subheadline)
+                    Spacer()
+                    Text("California")
+                        .withCustomFont(name: Constants.fonts.robotoLight, font: .subheadline)
+                }
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
                     .withCustomFont(name: Constants.fonts.robotoLight, font: .subheadline)
-                Spacer()
-                Text("California")
+        
+                Text("Descriptive text goes here.")
                     .withCustomFont(name: Constants.fonts.robotoLight, font: .subheadline)
+                
+                
             }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Spacer()
         }
-        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
