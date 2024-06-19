48b4d8c89a468b30f99527d04bbc1d0c5c6a4020
Abdallah Mahameed <a.mahameed0000@gmail.com>
2024-06-19 14:14:20 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by AbdallahMahameed on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
