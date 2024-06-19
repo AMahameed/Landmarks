af94a0f75250364a4cfd80eb57f743765fbf9ddb
Abdallah Mahameed <a.mahameed0000@gmail.com>
2024-06-19 17:09:13 +0300

    created new font modifier on Text

--- Landmarks/Extension/Font+Ext.swift
+++ Landmarks/Extension/Font+Ext.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct CustomFontModifier: ViewModifier {
+struct CustomFontAndSizeModifier: ViewModifier {
     let name: String
     let size: CGFloat
     
@@ -17,8 +17,27 @@ struct CustomFontModifier: ViewModifier {
     }
 }
 
-extension Text {
-    func withCustomFont(name: String, size: CGFloat) -> some View {
-        modifier(CustomFontModifier(name: name, size: size))
+struct CustomFontModifier: ViewModifier {
+    let name: String
+    let font: Font
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.custom(name, size: font == .title ? FontsWeight.title.rawValue : FontsWeight.subHeadline.rawValue))
     }
+}
+
+extension Text {
+    func withCustomFontAndSize(name: String, size: CGFloat) -> some View {
+        modifier(CustomFontAndSizeModifier(name: name, size: size))
+    }
+    
+    func withCustomFont(name: String, font: Font) -> some View {
+        modifier(CustomFontModifier(name: name, font: font))
+    }
+}
+
+enum FontsWeight: CGFloat {
+    case title = 32.0
+    case subHeadline = 20.0
 }
